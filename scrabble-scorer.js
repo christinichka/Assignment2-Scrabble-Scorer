a7ebf13d63ea66a03cffacda188d550be52864ef
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2021-03-01 19:01:46 -0600

    Update scrabble-scorer.js
    fixed vowelBonusScorer and also changed the return statement for the scorerPrompt function

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -2,8 +2,6 @@
 
 const input = require("readline-sync");
 
-let vowels = ['A', 'E', 'I', 'O', 'U'];
-
 const oldPointStructure = {
   1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
   2: ['D', 'G'],
@@ -34,23 +32,20 @@ function oldScrabbleScorer(word) {
 // your job is to finish writing these functions and variables that we've named //
 // don't change the names or your program won't work as expected. //
 
-let userWord = "";
+let word = "";
 
 // Prompt user for a word to be scored
 function initialPrompt() {
-  userWord = input.question (`Let's play some scrabble!\n\nEnter a word to score: `);
-  return oldScrabbleScorer(userWord);
+  word = input.question (`Let's play some scrabble!\n\nEnter a word to score: `);
+  return oldScrabbleScorer(word);
 }
-// console.log(initialPrompt(userWord));
+// console.log(initialPrompt(word));
 
 
 // Function takes a word as a parameter and returns a numerical score where each letter is worth 1 point
 function simpleScore(word) {
   word = word.toUpperCase();
-  let score = 0;
-  for (let i = 0; i < word.length; i++) {
-    score += 1;
-  }
+  let score = word.length;
   return score;
 }
 
@@ -59,7 +54,7 @@ function vowelBonusScore(word) {
   word = word.toUpperCase();
   let score = 0;
   for (let i = 0; i < word.length; i++) {
-    if (word[i] === vowels) {
+    if (word[i] === "A" || word[i] === "E" || word[i] === "I" || word[i] === "O" || word[i] === "U") {
       score += 3;
     } else {
       score += 1;
@@ -70,11 +65,11 @@ function vowelBonusScore(word) {
 
 
 function scrabbleScore(word) {
-  word = word.toUppercase();
+  word = word.toUpperCase();
   let score = 0;
   for (let i = 0; i < word.length; i++) {
-  score += 1;
-  }
+        score += newPointStructure[word[i]];
+      }
   return score;
 }
 
@@ -82,35 +77,35 @@ function scrabbleScore(word) {
 let simpleScoreObj = {
   name: 'Simple Score',
   description: 'Each letter is worth 1 point.',
-  scorerFunction: simpleScore
+  scoringFunction: simpleScore
 }
 
 let vowelBonusObj = {
   name: 'Bonus Vowels',
   description: 'Vowels are 3pts, consonants are 1 pt.',
-  scorerFunction: vowelBonusScore
+  scoringFunction: vowelBonusScore
 }
 
 let scrabbleObj = {
   name: 'Scrabble',
   description: 'The traditional scoring algorithm.',
-  scorerFunction: scrabbleScore
+  scoringFunction: scrabbleScore
 }
 
-// Array to organize the three scoring objects and their keys (name, description, scorerFunction)
+// Array to organize the three scoring objects and their keys (name, description, scoringFunction)
 const scoringAlgorithms = [simpleScoreObj, vowelBonusObj, scrabbleObj];
 
 // Let the user select which scoring algorithm to use when program scores a word.
 function scorerPrompt() {
-  let whichScorer = input.question(`Which scoring algorithm would you like to use?\n 0. Simple: One point per character\n 1. Vowel Bonus: Vowels are worth 3 points\n 2. Scrabble: Uses scrabble point system\nEnter 0, 1, or 2: `);
-  if (whichScorer === 0) {
-    console.log(`Score for '${userWord}' : ${scoringAlgorithms[0].scorerFunction(userWord)}`)
-  } else if (whichScorer === 1) {
-    console.log(`Score for '${userWord}' : ${scoringAlgorithms[1].scorerFunction(userWord)}`)
+  let whichScorer = input.question(`Which scoring algorithm would you like to use?\n 0 - Simple: One point per character\n 1 - Vowel Bonus: Vowels are worth 3 points\n 2 - Scrabble: Uses scrabble point system\nEnter 0, 1, or 2: `);
+  if (whichScorer == 0) {
+    console.log(`Score for '${word}' : ${scoringAlgorithms[0].scoringFunction(word)}`)
+  } else if (whichScorer == 1) {
+    console.log(`Score for '${word}' : ${scoringAlgorithms[1].scoringFunction(word)}`)
   } else {
-    console.log(`Score for '${userWord}' : ${scoringAlgorithms[2].scorerFunction(userWord)}`)
+    console.log(`Score for '${word}' : ${scoringAlgorithms[2].scoringFunction(word)}`)
   }
-  return scoringAlgorithms[whichScorer];
+  return whichScorer;
 }
 
 // Use the oldPointStructure to write a new function so that a single search will identify the point value for each letter.
@@ -119,7 +114,7 @@ function transform(oldPointStructure) {
   let newPointObj = {};
   for (key in oldPointStructure) {
     for (let i = 0; i < oldPointStructure[key].length; i++) {
-      newPointObj[(oldPointStructure[key][i].toLowerCase())] = Number(key);
+      newPointObj[(oldPointStructure[key][i]).toUpperCase()] = Number(key);
     }
   }
   oldPointStructure = newPointObj;
